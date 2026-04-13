7ca6dd27ecdf65d9af1b2c87a9cee0bd55aebb63
Krisha Raj <krisha.raj@gmail.com>
2026-04-13 14:41:26 +1000

    Create login webpage

--- db.py
+++ db.py
@@ -13,3 +13,16 @@ def GetAllGuesses():
     guesses = db.execute("SELECT * FROM Guesses").fetchall()
     db.close()
     return guesses
+
+def CheckLogin(username, password):
+    db = GetDB()
+    # Ask the database for a single user matching the provided name
+    user = db.execute("SELECT * FROM Users WHERE username=?", (username,)).fetchone()
+    # Do they exist?
+    if user is not None:
+        # OK they exist, is their password correct
+        if check_password_hash(user['password'], password):
+            # They got it right, return their details 
+            return user
+    # If we get here, the username or password failed.
+    return None
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import db
 
 app = Flask(__name__)
@@ -9,4 +9,27 @@ def Home():
     guessData = db.GetAllGuesses() # Note: the new line
     return render_template("index.html", guesses=guessData)
 
-app.run(debug=True, port=5000)
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session['id'] = user['id']
+            session['username'] = username
+
+            # Send them back to the homepage
+            return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
