6f749890dc3a316fb1036ee5d60adb81686ce960
Krisha Raj <krisha.raj@gmail.com>
2026-04-24 12:18:50 +1000

    Add comments

--- db.py
+++ db.py
@@ -1,60 +1,86 @@
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 def GetDB():
-    # Connect to the database and return the connection object
+    # Create a function to connect to the created database
     db = sqlite3.connect(".database/gtg.db")
+    #Allow the access of the rows in the database using their value
     db.row_factory = sqlite3.Row
+    #Return the database
     return db
 
 def GetAllGuesses():
-    # Connect, query all guesses and then return the data
+    # Run the GetDB() and connect to the database
     db = GetDB()
+    #Fetch all the data from the guesses table
+    #Join the guesses and user table using the user id
+    #Title case the username, by making the first letter uppercase and the rest of the username lowercase
+    #Order the data in descending order by date
     guesses = db.execute("""SELECT Guesses.date, Guesses.game, Guesses.score, UPPER(SUBSTR(Users.username, 1, 1)) || LOWER(SUBSTR(Users.username, 2)) AS username
                             FROM Guesses JOIN Users ON Guesses.user_id = Users.id
                             ORDER BY date DESC""").fetchall()
+    #Close the connection to the database
     db.close()
+    #Return the guesses
     return guesses
 
+#Create a function to check if the user provided the right login details
 def CheckLogin(username, password):
+    # Run the GetDB() and connect to the database
     db = GetDB()
-    # Ask the database for a single user matching the provided name
+    # Check if the username provided by the user matches a username in the database and then fetch that row from the table
     user = db.execute("SELECT * FROM Users WHERE username=?", (username,)).fetchone()
-    # Do they exist?
+    # Check if the above line returned a row from the table
     if user is not None:
-        # OK they exist, is their password correct
+        # If the user provided the right username, then check if the password is right
+        # Hash the password provided and check if it matches the password in the database
         if check_password_hash(user['password'], password):
             # They got it right, return their details 
+            #If the password matches then return the user details, including user id, username and password
             return user
-    # If we get here, the username or password failed.
+    # If the username or password don't exist in the database then return nothing
     return None
 
+#Create a function to register a new user into the PWA
 def RegisterUser(username, password):
-    # Check if they gave us a username and password
+    # Check that the username and password are both provided by the user
     if username is None or password is None:
+        #If one is not provided then return False
         return False
-    # Attempt to add them to the database
+    # Run the GetDB() and connect to the database
     db = GetDB()
+    #Hash the password provided by the user
     hash = generate_password_hash(password)
+    
+    
     try:
+        #Try and add the username and password into the database
         db.execute("INSERT INTO Users(username, password) VALUES(?, ?)", (username, hash,))
         db.commit()
+    #If the username already exists then the below error will pop up, because each username must be unique in the database
     except sqlite3.IntegrityError as e:
-        print(f"Error: {e}. This username is already taken.")
+        #If this error pops up, then except the error
+        # Then rollback the previous commit and undo all changes, to ensure that the database is not locked down due to the error
         db.rollback()
+        #Return False; the username and password were not added into the database
         return False
+    #Return true if this error is not picked up; the username and password were added into the database
     return True
 
-
+#Create a function to add user guesses into the database
 def AddGuess(user_id, date, game, score):
-    # Check if any boxes were empty
-    if date is None or game is None:
+    # Check and ensure that the none of the entries are empty
+    if date is None or game is None or score is None:
         return False
-    # Get the DB and add the guess
+    # Run the GetDB() and connect to the database
     db = GetDB()
     
+    #Add the user_id, date, game and score into the guesses database
     db.execute("INSERT INTO Guesses(user_id, date, game, score) VALUES (?, ?, ?, ?)",
                (user_id, date, game, score,))
+    #Commit the changes to the database
     db.commit()
-
+    
+    #Return true signalling that the guess added into the table
     return True
--- main.py
+++ main.py
@@ -1,84 +1,117 @@
 from flask import Flask, render_template, request, redirect, session, send_from_directory, flash
 import db
 
+#Activate Flask app
 app = Flask(__name__)
+#Create app secret key
 app.secret_key = "gtg"
 
+
+#Create home page for PWA
 @app.route("/")
 def Home():
-    username = 'there'
+    #Create blank username and guess data
+    username = ''
     guessData = ""
+    #Check if the user is logged in
     if session.get('username'):
+        #If ther user is logged in then title case their username
         username = session['username'].title()
+        #Fetch all guesses from the guesses database using GetAllGuesses function from db.py
         guessData = db.GetAllGuesses() # Note: the new line
+    #Render the index.html page, and send through the parameters guessData and username
+    #The guessdata will display the table
+    #The username, if logged in, will display the username on the home page
     return render_template("index.html", guesses=guessData, name=username)
 
+#Create the login page for the PWA, the get request will retrieve data(the login page) and the post request will send through the username and password
 @app.route("/login", methods=["GET", "POST"])
 def Login():
+    #Check if the user is already logged in
     if session.get('username') != None:
+        #If the user is already logged in then redirect them to the home page
         return redirect("/")
+    #Check if the user had submitted the form
     if request.method == "POST":
+        #Get the username and password the user submitted
         username = request.form['username']
+        #Lowercase the username; the username in the database is stored in lowercase
         username = username.lower()
         password = request.form['password']
 
-        # Did they provide good details
+        # Check if the username and password are in the database; using the CheckLogin method from the db.py
         user = db.CheckLogin(username, password)
+        #Check if the user is logged in
         if user:
-            # Yes! Save their username and id then
+            #Save their id; using the id in the database
             session['id'] = user['id']
+            #Save their username to the session; will be used later to check if the user is logged in or not
             session['username'] = username
 
-            # Send them back to the homepage
+            # Redirect user to homepage if they are logged in
             return redirect("/")
         else:
+            #If they are not logged in then flash the message
             flash("Your username or password was incorrect")
+    #Render the login.html page
     return render_template("login.html")
 
+#Create the logout page
 @app.route("/logout")
 def Logout():
+    #Clear the session; will remove previously stored user id and username
     session.clear()
+    #Redirect user to homepage
     return redirect("/")
 
+#Create the registration page, the get request will retrieve the data(register page) and the post request will send through the username and password
 @app.route("/register", methods=["GET", "POST"])
 def Register():
+    #If the user is already logged in then redriect them to the home page
     if session.get('username') != None:
         return redirect("/")
-    # If they click the submit button, let's register
+    #Check if the user has submitted the form
     if request.method == "POST":
+        #Retrieve the username from the form
         username = request.form['username']
+        #Lowercase the username, to store into the database in lowercase
         username = username.lower()
+        #Retrieve the password
         password = request.form['password']
 
-        # Try and add them to the DB
+        #Add the username and password to the database
         if db.RegisterUser(username, password):
-            # Success! Let's go to the homepage
+            # If the username and password are added then redirect them to the homepage
             return redirect("/")
         else:
+            # If the username and password aren't added to the username, flash the message the username is already taken
             flash("This username is already taken")
             
-        
+    #Render the template register.html
     return render_template("register.html")
 
+#Create the add guess page for the PWA, the get request will retrieve the data(add guess data) and the post request will send through the guess details
 @app.route("/add", methods=["GET","POST"])
 def Add():
-    # Check if they are logged in first
+    # Check if the user is logged in
     if session.get('username') == None:
+        #If they aren't logged in, redirect them back to the homepage
         return redirect("/")
 
-    # Did they click submit?
+    # Check if the user submitted the form
     if request.method == "POST":
+        #Collect the data the user submitted, including the user id from the session id, the date, the game name and the score
         user_id = session['id']
         date = request.form['date']
         game = request.form['game']
         score = request.form['score']
-        # Send the data to add our new guess to the db
+        # Add the data to the guesses database using the AddGuess function from db.py
         db.AddGuess(user_id, date, game, score)
+    #Render the template add.html
     return render_template("add.html")
 
-@app.route('/static/manifest.json')
-def manifest():
-    return send_from_directory('static', 'manifest.json', mimetype='application/manifest+json')
-
+# if the is being run by python then the __name__ become __main__
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    #If the file is run through python then run the Flask app on the port 5000 on the local host
+    #Debug mode is off
+    app.run(debug=False, port=5000)
